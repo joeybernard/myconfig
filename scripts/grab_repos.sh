7f3ba72aad3db475af40aefededd4c21bb1495a6
Joey Bernard <druid.bernard@gmail.com>
2018-06-04 16:14:39 -0300

    Added some entries to .gitignore. Added some chain repos to the list in grab_repos.sh. Added a script to setup git on a new system.

--- scripts/grab_repos.sh
+++ scripts/grab_repos.sh
@@ -9,6 +9,8 @@ cd ~/repos
 git clone https://joeybernard@bitbucket.org/joeybernard/lwang.git
 git clone https://github.com/joeybernard/writing.git
 git clone https://github.com/joeybernard/joeybernard.github.io.git
+git clone https://git.chain-project.net/Joey/jbernard.git
+git clone https://git.chain-project.net/RadioPhysics/GeneralInfo.git
 
 cd $curr_dir
 
