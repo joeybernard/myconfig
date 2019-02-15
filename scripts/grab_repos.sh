72333f8f0a86a1580d3667fd9b773cc3c37caf75
Joey Bernard <druid.bernard@gmail.com>
2019-02-14 20:34:22 -0400

    Changed some of the repos that get installed initially.

--- scripts/grab_repos.sh
+++ scripts/grab_repos.sh
@@ -6,11 +6,12 @@ fi
 
 curr_dir=`pwd`
 cd ~/repos
-git clone https://joeybernard@bitbucket.org/joeybernard/lwang.git
+
 git clone https://github.com/joeybernard/writing.git
 git clone https://github.com/joeybernard/joeybernard.github.io.git
 git clone https://git.chain-project.net/Joey/jbernard.git
 git clone https://git.chain-project.net/RadioPhysics/GeneralInfo.git
+git clone https://github.com/MotusWTS/motusServer.git
 
 cd $curr_dir
 
