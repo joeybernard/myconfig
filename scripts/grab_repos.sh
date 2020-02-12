1eb83865564a4235824e7c22d0ca4216f86a628c
Joey Bernard <druid.bernard@gmail.com>
2020-02-12 13:32:31 -0400

    Added files/folders not to include from android.
    Removed some repos that I don't need right now.

--- scripts/grab_repos.sh
+++ scripts/grab_repos.sh
@@ -17,15 +17,15 @@ git clone https://git.chain-project.net/RadioPhysics/GeneralInfo.git
 
 git clone https://github.com/MotusWTS/motusServer.git
 git clone https://github.com/MotusWTS/find_tags.git
-git clone https://github.com/MotusWTS/MotusRBook.git
-git clone https://github.com/MotusWTS/Documentation.git
-git clone https://github.com/MotusWTS/motus.git
-git clone https://github.com/MotusWTS/motusClient.git
-git clone https://github.com/MotusWTS/motus-metadata-history.git
-git clone https://github.com/MotusWTS/motusData.git
+#git clone https://github.com/MotusWTS/MotusRBook.git
+#git clone https://github.com/MotusWTS/Documentation.git
+#git clone https://github.com/MotusWTS/motus.git
+#git clone https://github.com/MotusWTS/motusClient.git
+#git clone https://github.com/MotusWTS/motus-metadata-history.git
+#git clone https://github.com/MotusWTS/motusData.git
 git clone https://github.com/MotusWTS/program_options.git
 
-git clone https://github.com/MotusDev/MotusAPI.git
+#git clone https://github.com/MotusDev/MotusAPI.git
 
 cd $curr_dir
 
