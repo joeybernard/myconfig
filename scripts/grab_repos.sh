9746acfc25fbfe29cd9fce69b847d08c5f589cc4
Joey Bernard <druid.bernard@gmail.com>
2022-01-13 19:46:37 -0400

    Added resharper to gitignore; added til repo to installation list

--- scripts/grab_repos.sh
+++ scripts/grab_repos.sh
@@ -10,6 +10,7 @@ cd ~/repos
 git clone https://github.com/joeybernard/writing.git
 git clone https://github.com/joeybernard/joeybernard.github.io.git
 git clone https://github.com/joeybernard/scratch.git
+git clone https://github.com/joeybernard/til.git
 
 #git clone https://git.chain-project.net/Joey/jbernard.git
 #git clone https://git.chain-project.net/RadioPhysics/GeneralInfo.git
