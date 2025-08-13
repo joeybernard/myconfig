17874afdc66156a39204fa998a4ba08358895368
Joey Bernard <druid.bernard@gmail.com>
2025-08-13 09:39:32 -0300

    Removed some unneeded software

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -1,12 +1,12 @@
 # Initial software installation
 sudo apt-get install git 
-sudo apt install glances 
+#sudo apt install glances 
 sudo apt install fortune-mod fortunes fortunes-bofh-excuses fortunes-off
 
 # nikola software
 sudo apt-get install python3-pip
-pip3 install nikola typogrify pygal pyphen ghp-import2
+#pip3 install nikola typogrify pygal pyphen ghp-import2
 
 # the fuck
-sudo apt-get install thefuck
+#sudo apt-get install thefuck
 
